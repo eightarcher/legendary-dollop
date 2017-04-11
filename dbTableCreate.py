bee6ff05d9590a2a54ee6d1a505782a40f5394f0
eightarcher <mikeschaff@gmail.com>
2017-04-11 16:34:31 -0400

    backing up

--- dbTableCreate.py
+++ dbTableCreate.py
@@ -3,3 +3,17 @@ def create_table():
 
 create_table()
 
+def enter_data():
+    c.execute("INSERT INTO example VALUES ('Python', '2.7', 'Beginner')")
+    c.execute("INSERT INTO example VALUES ('Python', '3.3', 'Expert')")
+    c.execute("INSERT INTO example VALUES ('Python', '3.6', 'Intermediate')")
+
+def enter_dynamic_data():
+    variety = input("What flavah? ")
+    vineyard = input("Where from? ")
+    year = input("Which year? ")
+
+    c.execute("INSERT INTO wino (Variety, Vineyard, Year) VALUES (?,?,?)", (variety, vineyard, year))
+
+    conn.commit()
+    
